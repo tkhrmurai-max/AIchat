b1f8fa3af77cba65974ebad518e63e56a3450622
tkhrmurai-max <tkhr.murai@ur-cloud.jp>
2026-01-13 13:57:38 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // ↓↓↓ リポジトリ名（住所）の設定
+      // ↓↓↓ Alchatの場合はこれが正解です！
       base: '/AIchat/', 
       
       server: {
@@ -13,14 +13,14 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      // ↓↓↓ APIキーをアプリに渡す設定
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          // ↓↓↓ ここを修正しました（__dirnameを使わない書き方）
+          '@': path.resolve('.'), 
         }
       }
     };
